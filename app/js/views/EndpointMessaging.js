e93d680a493f8f26910b04eafc9e41b63d498b9f
Tyson <tysoncadenhead@gmail.com>
2014-09-12 15:40:50 -0500

    Added unit tests

--- app/js/views/EndpointMessaging.js
+++ app/js/views/EndpointMessaging.js
@@ -2,14 +2,87 @@ var messagingView = (function ($, App) {
 
     'use strict';
 
+    /**
+     * The Endpoint Messaging View
+     */
     return function (options) {
 
         var client, connection, $el;
 
-        options.renderTo = options.renderTo || '#endpoint-messaging';
-
         return {
 
+            /**
+             * A callback fired when the form is submitted to post a new message
+             */
+            postMessage: function (e) {
+
+                // Prevent the form from posting back
+                e.preventDefault();
+
+                var $message = $el.find('.em-message__text'),
+                    message = $message.val();
+                this.sendMessage(message);
+
+                // Reset the value of the message field to by empty
+                $message.val('');
+            },
+
+            /**
+             * Appends a message to the thread
+             */
+            renderMessage: function (message) {
+
+                // Get the contents of the inline template
+                var tmpl = $('#endpoint-thread-message').html(),
+
+                    // Applies the data to the template using John Resig's micro-templating
+                    html = $.tmpl(tmpl, {
+                        message: message
+                    });
+
+                $el.find('.em-thread').append(html);
+            },
+
+            /**
+             * Appends a reply message to the thread
+             */
+            renderReply: function (evt) {
+
+                // Get the contents of the inline template
+                var tmpl = $('#endpoint-thread-reply').html(),
+
+                    // Applies the data to the template using John Resig's micro-templating
+                    html = $.tmpl(tmpl, {
+                        username: options.connectTo,
+                        message: evt.message.message
+                    });
+
+                $el.find('.em-thread').append(html);
+            },
+
+            /**
+             * Renders the main template to hold the thread
+             */
+            renderMainTemplate: function () {
+
+                // Get the contents of the inline template
+                var tmpl = $('#endpoint-thread').html(),
+
+                    // Applies the data to the template using John Resig's micro-templating
+                    html = $.tmpl(tmpl, options);
+
+                // The element is defined outside of this scope so we can access it in any method of this view
+                $el = $(html);
+
+                // Bind the form submission to our postMessage method
+                $el.find('.em-message').bind('submit', this.postMessage.bind(this));
+
+                $('#endpoint-messaging').append($el);
+            },
+
+            /**
+             * Sends the message over respoke and appends the message to the thread of the endpoint who posted it
+             */
             sendMessage: function (message) {
                 connection.sendMessage({
                     message: message
@@ -17,70 +90,31 @@ var messagingView = (function ($, App) {
                 this.renderMessage(message);
             },
 
-            postMessage: function (e) {
-                e.preventDefault();
-
-                var $message = $el.find('.em-message__text'),
-                    message = $message.val();
-                this.sendMessage(message);
-
-                $message.val('');
-            },
-
-            getTemplate: function (options) {
-                var tmpl = $(options.el).html(),
-                    html = $.tmpl(tmpl, options.data);
-                return html;
-            },
-
-            renderMessage: function (message) {
-                var html = this.getTemplate({
-                    el: '#endpoint-thread-message',
-                    data: {
-                        message: message
-                    }
-                });
-                $el.find('.em-thread').append(html);
-            },
-
-            renderReply: function (evt) {
-                var html = this.getTemplate({
-                    el: '#endpoint-thread-reply',
-                    data: {
-                        username: options.connectTo,
-                        message: evt.message.message
-                    }
-                });
-                $el.find('.em-thread').append(html);
-            },
-
-            renderMainTemplate: function () {
-                var html = this.getTemplate({
-                    el: '#endpoint-thread',
-                    data: options
-                });
-                $el = $(html);
-                $el.find('.em-message').bind('submit', this.postMessage.bind(this));
-                $(options.renderTo).append($el);
-            },
-
+            /**
+             * Creates a connection between the client and the other endpoint
+             */
             createConnection: function () {
                 connection = App.models.endpoint({
                     id: options.connectTo,
                     client: client,
-                    onMessage: this.renderReply.bind(this)
+                    onMessage: this.renderReply
                 });
             },
 
-            onConnection: function (obj) {
-                client = obj;
+            /**
+             * When an endpoint is connected, we will set the client data and create a connection between the client and the other endpoint
+             */
+            onConnection: function (_client) {
+                client = _client;
                 this.createConnection();
             },
 
+            /**
+             * Kicks off the view by attempting to initialize the client and rendering the main template for that client 
+             */
             init: function () {
                 this.renderMainTemplate();
                 App.models.client(options.username, this.onConnection.bind(this));
-                return this;
             }
 
         };
--- test/specs/views.endpointMessaging.spec.js
+++ test/specs/views.endpointMessaging.spec.js
@@ -7,18 +7,37 @@ describe('The Endpoint Messaging View', function () {
         });
     });
 
-    /*describe('when we connect to a client', function () {
-        it('should make a connection', function () {
-            var messageSent = sinon.spy();
-            sinon.stub(App.models, 'endpoint', function () {
-                return {
-                    sendMessage: messageSent
-                }
+    describe('when the client is connected', function () {
+        it('should set the client on the view', function (done) {
+            var endpoint = App.models.endpoint;
+            App.models.endpoint = function (obj) {
+                assert.equal(obj.client.myString, 'hi');
+                done();
+            };
+            this.view.onConnection({
+                myString: 'hi'
             });
-            sinon.spy(this.view, 'sendMessage');
-            this.view.createConnection();
-            App.models.endpoint.restore();
+            App.models.endpoint = endpoint;
         });
-    });*/
+    });
+
+    describe('when we send a message', function () {
+        it('should send the message to the model', function (done) {
+            var endpoint = App.models.endpoint;
+            App.models.endpoint = function (obj) {
+                return {
+                    sendMessage: function (data) {
+                        assert.equal(data.message, 'test message');
+                        done();
+                    }
+                }
+            };
+            this.view.renderMessage = sinon.stub();
+            this.view.onConnection({
+                myString: 'hi'
+            });
+            this.view.sendMessage('test message');
+        });
+    });
 
 });
